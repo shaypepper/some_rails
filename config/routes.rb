bcbde168835845948070f287efb0ce9e9d6d318e
Shay Culpepper <shaypepper@gmail.com>
2016-08-18 14:28:29 -0500

    fixed some things

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'zebra#index'
   resources :zebras
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
